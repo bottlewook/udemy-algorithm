c7f0d2800bef7ee1c71c806d9dce4e0f67d46eae
wookki <bwlimtony@gmail.com>
2023-10-03 17:17:11 +0900

    feat: recursiveRange.js

---
+++ Recursive/recursiveRange.js
@@ -0,0 +1,10 @@
+// 숫자를 받으면 0부터 함수에 전달된 숫자까지의 모든 숫자를 더하는 recursiveRange라는 함수를 작성하시오.
+
+// SAMPLE INPUT/OUTPUT
+// recursiveRange(6) // 21
+// recursiveRange(10) // 55
+
+function recursiveRange(num) {
+  if (num === 1) return 1;
+  return num + recursiveRange(--num);
+}
