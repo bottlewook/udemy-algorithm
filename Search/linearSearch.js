6bd097721f05af3d5c2b20fd16f10d04e5ddff14
wookki <bwlimtony@gmail.com>
2023-10-03 19:21:06 +0900

    feat: linearSearch.js

---
+++ Search/linearSearch.js
@@ -0,0 +1,8 @@
+// 배열과 값을 받아들이고 그 값이 배열 안에 존재하는 경우 그 인덱스(0, 1, 2, ... 중 몇 번째에 위치하는지)를 반환하는 linearSearch라는 함수를 작성합니다. 값이 배열 안에 존재하지 않으면 -1을 반환합니다.
+
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) return i;
+  }
+  return -1;
+}
