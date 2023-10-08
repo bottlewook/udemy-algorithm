abd19caa3afd9bbabe87736654e2b313e7149dd4
wookki <bwlimtony@gmail.com>
2023-10-09 01:35:27 +0900

    feat: bubbleSort.js

---
+++ SortingAlgorithm/bubbleSort.js
@@ -0,0 +1,51 @@
+// 버블정렬이란 접한 2개의 레코드를 비교하여 크기가 순서대로 되어 있지 않으면 서로 교환하는 정렬 방식
+// 시각복잡도는 O(n^2)
+
+// 최적화 되지 않은 방법
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+// 최적화 1
+function bubbleSort1(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+function bubbleSort2(arr) {
+  let noSwaps = true;
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
+}
+
+console.log(bubbleSort([37, 45, 28, 9]));
