c8b1a1f9aa3ebfc8124f52ba121c8b7a279e8437
wookki <bwlimtony@gmail.com>
2023-10-12 00:50:45 +0900

    feat: SinglyLinkedList-shift

--- List/singlyLinkedList.js
+++ List/singlyLinkedList.js
@@ -41,6 +41,17 @@ class SinglyLinkedList {
     }
     return current;
   }
+
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
 }
 
 const list = new SinglyLinkedList();
