8372741813810de7fa04bd609e9504bb2838ba54
wookki <bwlimtony@gmail.com>
2023-10-11 22:41:52 +0900

    feat: SinglyLinkedList.js

---
+++ List/singlyLinkedList.js
@@ -0,0 +1,32 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = 0;
+    this.tail = 0;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+const list = new SinglyLinkedList();
+list.push("HELLO");
+list.push("GOODBYE");
+list.push(99);
+console.log(list);
