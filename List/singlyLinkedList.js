82b0b8a310df863656562c251a33510d79be5a2a
wookki <bwlimtony@gmail.com>
2023-10-12 00:22:12 +0900

    feat: SinglyLinkedList-pop

--- List/singlyLinkedList.js
+++ List/singlyLinkedList.js
@@ -23,10 +23,32 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
 
 const list = new SinglyLinkedList();
 list.push("HELLO");
 list.push("GOODBYE");
 list.push(99);
+console.log(list);
+list.pop();
+list.pop();
+list.pop();
 console.log(list);
