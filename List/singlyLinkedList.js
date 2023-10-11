e06b389a695c21a80c3e5bcc2cee319fd61792ce
wookki <bwlimtony@gmail.com>
2023-10-12 01:07:27 +0900

    feat: SinglyLinkedList-unshift

--- List/singlyLinkedList.js
+++ List/singlyLinkedList.js
@@ -52,6 +52,18 @@ class SinglyLinkedList {
     }
     return currentHead;
   }
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return newNode;
+  }
 }
 
 const list = new SinglyLinkedList();
