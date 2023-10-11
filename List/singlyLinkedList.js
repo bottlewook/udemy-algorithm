5b456b8dc169b5bd98e04f244bab30b296950123
wookki <bwlimtony@gmail.com>
2023-10-12 03:23:16 +0900

    feat: SinglyLinkedList-reverse

--- List/singlyLinkedList.js
+++ List/singlyLinkedList.js
@@ -108,6 +108,20 @@ class SinglyLinkedList {
     this.length--;
     return true;
   }
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next = null;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
 }
 
 const list = new SinglyLinkedList();
