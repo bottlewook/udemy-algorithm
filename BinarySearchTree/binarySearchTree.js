aa09312d9f574b4a8a30a288cce3a6099a8f7c39
wookki <bwlimtony@gmail.com>
2023-10-13 00:27:35 +0900

    feat: binarySearchTree-DPSPostOrder

--- BinarySearchTree/binarySearchTree.js
+++ BinarySearchTree/binarySearchTree.js
@@ -74,6 +74,16 @@ class BinarySearchTree {
     traverse(this.root);
     return data;
   }
+  DFSPostOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node);
+    }
+    traverse(this.root);
+    return data;
+  }
 }
 
 const tree = new BinarySearchTree();
