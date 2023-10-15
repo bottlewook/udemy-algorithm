d54997c777e9a56179a25a73171d9324c1304b46
wookki <bwlimtony@gmail.com>
2023-10-16 01:33:04 +0900

    feat:graph-breadthFirst

--- Graph/graph.js
+++ Graph/graph.js
@@ -66,6 +66,7 @@ class Graph {
     const result = [];
     const visited = {};
     let currentVertex;
+    visited[start] = true;
 
     while (queue.length) {
       currentVertex = queue.shift();
