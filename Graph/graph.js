2e9725667d3452f24f1b1eb72c9f55d738b3984e
wookki <bwlimtony@gmail.com>
2023-10-16 01:30:22 +0900

    feat:graph-breadthFirst

--- Graph/graph.js
+++ Graph/graph.js
@@ -61,6 +61,24 @@ class Graph {
     }
     return result;
   }
+  breadthFirst(start) {
+    const queue = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+
+    while (queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex);
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
 
 const g = new Graph();
